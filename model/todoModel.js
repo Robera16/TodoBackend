6662cfc11655646cb00c0678d7a86efefc55a5a7
Robera16 <roberaworknehrobwork@gmail.com>
2024-02-07 21:20:09 +0300

    todo backend

---
+++ model/todoModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const todoSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
